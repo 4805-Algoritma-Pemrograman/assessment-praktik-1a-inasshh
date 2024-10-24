4bb610d9a554fe86dd8510b4be2483299431c8f3
inasshh <inasaulia00@gmail.com>
2024-10-24 07:55:16 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,23 @@
 public class Nomor1{
     public static void main(String[] args){
+        double totalJamLembur;
+        double honor Lembur = 0;
+        Scanner input = new Scanner(System.in);
 
+        //input total jam lembur
+        System.out.println("TotalJamLembur: ");
+        totalJamLembur = input.nextdouble();
+
+        //Hitung honor lembur
+        if (totalJamLembur <= 16) {
+            honorLembur = totalJamLembur * 10000;
+        } else if (totalJamLembur > 16) {
+            honorLembur = totalJamLembur * 15000;
+        }
+        
+        
+        
+
+        
     }
 }
