3d09f257ceab744ff64cf809ac3ca42d51caa408
inasshh <inasaulia00@gmail.com>
2024-10-24 08:14:44 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,14 @@
 public class Nomor2{
     public static void main(String[] args){
+        Scanner scanner = new Scanner .nextLine(System.in);
+
+        //input nama driver
+        System.out.println("nama driver?[Bliau/Mereka/Kita]");
+        String driver = scanner.nextLine();
+
+        //menghitung omset
+        if 
+        
 
     }
 }
